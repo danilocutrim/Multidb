f78b7dc1e1dd028476c9179bace03542dd65d0f7
danilocutrim <danilocutrim@live.com>
2019-04-08 22:33:02 -0300

    implementados update and delete mongodb

--- src/tests/mongodb.test.js
+++ src/tests/mongodb.test.js
@@ -9,6 +9,11 @@ const MOCK_HEROI_DEFAULT = {
     nome: `homem aranha-${Date.now()}`,
     poder: 'teia'
 }
+const MOCK_HEROI_ATUALIZAR = {
+    nome: `patolino-${Date.now()}`,
+    poder: 'velocidade'
+}
+let MOCK_HEROI_ID = ''
 
 const context = new Context(new Mongodb())
 describe('Mongodb Suite de testes mongo', function (){
@@ -24,6 +29,8 @@ describe('Mongodb Suite de testes mongo', function (){
     it('cadastrar mongo ', async () =>{
         const {nome, poder} = await context.create(MOCK_HEROI_CADASTRAR)
         await context.create(MOCK_HEROI_DEFAULT)
+        const result = await context.create(MOCK_HEROI_ATUALIZAR)
+        MOCK_HEROI_ID = result._id
         assert.deepEqual({nome, poder}, MOCK_HEROI_CADASTRAR)
     })
     it('listar herois mongo', async ()=>{
@@ -33,4 +40,16 @@ describe('Mongodb Suite de testes mongo', function (){
          }
         assert.deepEqual(result,MOCK_HEROI_DEFAULT)
     })
+    it('atualizar mongo', async ()=>{
+        const result = await context.update(MOCK_HEROI_ID,{
+            nome:'danilo'
+        })
+        assert.deepEqual(result.nModified,1)
+    })
+    it('remover mongo', async()=>{
+        const result = await context.delete(MOCK_HEROI_ID)
+        console.log(result.n)
+        assert.deepEqual(result.n, 1)
+        console.log()
+    } )
 })
